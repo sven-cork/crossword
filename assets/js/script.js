7baf02d64c9776fc6515162509c360d7fb9ac84a
sven-cork <linslus81@gmail.com>
2023-01-05 07:53:17 +0000

    Add code to test keydown and update of value box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 
+document.getElementsByClassName("input-box").addEventListener("keydown", updateResult);
+
+function updateResult() {
+    let keyPressed = document.getElementsByClassName("input-box").innerText;
+    document.getElementById("key-result").innerText = keyPressed;
+}
+
+
+
 
 console.log("start of project2")
