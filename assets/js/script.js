cd918c5b8da40b020ee332acc11813d47c92477f
sven-cork <linslus81@gmail.com>
2023-01-05 13:11:10 +0000

    Edit function to input keyboard letter entered in to object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
+
+
 function updateResult() {
+    let letterList = [];
+    let letterObject = {First:"something"}
 
     let keyPressed = document.getElementById("key-input").value;
     console.log("keyPress", keyPressed);
     document.getElementById("key-result").innerText = keyPressed;
+    letterList.push(keyPressed);
     console.log("start of project2");
+    console.log(letterList);
+    console.log("letterList logged?")
+    letterObject.second = keyPressed;
+    console.log(letterObject);
 }
 
 document.getElementById("submit-button").addEventListener("click", updateResult);
