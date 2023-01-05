add93d9c64c42d5c1ec5a00a16a13c8516c903a3
sven-cork <linslus81@gmail.com>
2023-01-05 17:40:10 +0000

    Create function compareEntries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,40 @@
 
 
+function compareEntries() {
+    let correctFirstDown = {1 : "S", 2 : "H", 3 : "H"};
+
+    let userEnteredFirstDown = {}
+
+    //User entered value and cell number for first cell
+    let firstKeyPressed = document.getElementById("first-key-input").value;
+    let firstDivNumber = document.getElementById("first-divsub").textContent;
+    console.log("Div Number from compareEntries function:", firstDivNumber);
+    
+    //User entered value and cell number for second cell
+    let secondKeyPressed = document.getElementById("sixth-key-input").value;
+    let divNumber = document.getElementById("sixth-divsub").textContent;
+    console.log("Div Number from compareEntries function:", divNumber);
+
+    console.log(correctFirstDown);
+}
+
 function updateResult() {
     let letterList = [];
     let letterObject = {First:"something"}
 
-    let keyPressed = document.getElementById("key-input").value;
-    let divNumber = document.getElementById("firstdiv").innerText;
+    let firstKeyPressed = document.getElementById("first-key-input").value;
+    let divNumber = document.getElementById("first-divsub").textContent;
     console.log("Div Number:", divNumber);
-    console.log("keyPress", keyPressed);
-    document.getElementById("key-result").innerText = keyPressed;
-    letterList.push(keyPressed);
+    console.log("keyPress", firstKeyPressed);
+    document.getElementById("key-result").innerText = firstKeyPressed;
+    letterList.push(firstKeyPressed);
     console.log("start of project2");
     console.log(letterList);
     console.log("letterList logged?")
-    letterObject.second = keyPressed;
+    letterObject.divNumber = firstKeyPressed;
     console.log(letterObject);
+
+    compareEntries()
 }
 
 document.getElementById("submit-button").addEventListener("click", updateResult);
