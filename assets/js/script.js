c5f0e1e6c4b40d30802a4f6d85de691154f1de6b
sven-cork <linslus81@gmail.com>
2023-01-06 08:21:30 +0000

    Add correctAnswer function holding array with correct crossword

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-
+function correctCrossword() {
+    let correctCrossword = [['S','T','A','R'], ['H','O','P','E'], ['H','Y','P','E','R'], ['E','L','S','E'], ['D','E','E','M']];
+    return correctCrossword;
+}
 
 function compareEntries() {
     let correctFirstDown = {1 : "S", 2 : "H", 3 : "H"};
@@ -9,7 +12,14 @@ function compareEntries() {
     let firstKeyPressed = document.getElementById("first-key-input").value;
     let firstDivNumber = document.getElementById("first-divsub").textContent;
     console.log("Div Number from compareEntries function:", firstDivNumber);
-    
+    if (firstKeyPressed === "E") {
+        let correctAnswer = correctCrossword();
+        console.log("List of answered being displayed below:");
+        console.log(correctAnswer);
+        console.log("Displaying first letter in list below:");
+        console.log(correctAnswer[0][0]);
+    }
+
     //User entered value and cell number for second cell
     let secondKeyPressed = document.getElementById("sixth-key-input").value;
     let secondDivNumber = document.getElementById("sixth-divsub").textContent;
